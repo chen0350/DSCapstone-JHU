fc36b6960b897552e7053a153f17ce94c576b6ba
chen0350 <bryan_giggs@hotmail.com>
2020-07-22 08:13:38 +0800

    First commit

---
+++ server.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(tm)
+library(RWeka)
+library(stringr)
+
+shinyServer(
+    function(input, output) {
+        setwd("/Users/michelle/Desktop/DataScience/DSCapstone-JHU")
+        bigrams<-readRDS("bigrams_all.rds")
+        trigrams<-readRDS("trigrams_all.rds")
+        
+        T1 <- eventReactive(input$predictButton, {
+            input$phraseBox
+        })
+        
+        T2 <- eventReactive(input$predictButton, {
+            phrase <- input$phraseBox
+            source("MindReader.R")
+            nextWord <- predict(phrase,input$noOfWords)
+        })
+        
+        output$Text1 <- renderText({
+            T1()
+        })
+        
+        output$Text2 <- renderPrint({
+            cat(T2(), sep = "\n")
+        })
+        
+    }
+)
